ba98db0d747859700188f7ff512f182b473f0c38
Jamie Davies <jamie@jamiedavies.me>
2014-08-04 10:53:48 +0100

    Added combo-box for network interface.

--- jampacket.py
+++ jampacket.py
@@ -4,6 +4,7 @@ __author__  = 'Jamie Davies'
 __version__ = '0.0.1'
 
 import gtk
+import netifaces as ni
 
 
 class JamPacketApp(gtk.Window):
@@ -17,6 +18,13 @@ class JamPacketApp(gtk.Window):
         self.set_resizable(False)
         self.set_position(gtk.WIN_POS_CENTER)
 
+        cb_iface = gtk.combo_box_new_text()
+        cb_iface.connect('changed', self.iface_changed)
+        for iface in ni.interfaces():
+            if iface.startswith('eth') or iface.startswith('wlan'):
+                cb_iface.append_text('{} - {}'.format(
+                    iface, ni.ifaddresses(iface)[ni.AF_INET][0]['addr']))
+
         cb_proto = gtk.combo_box_new_text()
         cb_proto.connect('changed', self.protocol_changed)
         cb_proto.append_text('ARP')
@@ -27,12 +35,17 @@ class JamPacketApp(gtk.Window):
         table.set_row_spacings(10)
         table.set_col_spacings(10)
 
-        table.attach(gtk.Label("Protocol"), 0, 1, 0, 1)
-        table.attach(cb_proto, 1, 2, 0, 1)
+        table.attach(gtk.Label("Interface"), 0, 1, 0, 1)
+        table.attach(cb_iface, 1, 2, 0, 1)
+        table.attach(gtk.Label("Protocol"), 0, 1, 1, 2)
+        table.attach(cb_proto, 1, 2, 1, 2)
 
         self.add(table)
 
         self.show_all()
+
+    def iface_changed(self, widget):
+        print 'Interface changed to {}'.format(widget.get_active_text())
 
     def protocol_changed(self, widget):
         print 'Changed to {}'.format(widget.get_active_text())
